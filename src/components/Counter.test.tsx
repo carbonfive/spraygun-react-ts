bbd154a7c36a76e4ce207471b4be0602223b3922
Matt Brictson <mattb@carbonfive.com>
2021-11-01 09:32:41 -0700

    render is not allowed in `beforeEach`, so move it

--- src/components/Counter.test.tsx
+++ src/components/Counter.test.tsx
@@ -2,18 +2,16 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Counter from "./Counter";
 
-beforeEach(() => {
-  render(<Counter />);
-});
-
 describe("initial state", () => {
   it("renders 0", () => {
+    render(<Counter />);
     expect(screen.getByText("0")).toBeInTheDocument();
   });
 });
 
 describe("onClick", () => {
   it("increments when clicked", async () => {
+    render(<Counter />);
     await userEvent.click(screen.getByRole("button"));
     expect(screen.getByText("1")).toBeInTheDocument();
 
